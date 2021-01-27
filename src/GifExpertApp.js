fc70f1adc9e4a32a0a591e21d739f06152fef753
Adriana Venegas <adrivegas08@gmail.com>
2021-01-27 11:25:35 -0500

    Creación de lista de categorías

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const GifExpertApp = () => {
+
+    // const categories = ['Gamora', 'Iron Man', 'Thor']. *****Así no se debe hacer****
+    const [categories, setCategories] = useState(['Gamora', 'Iron Man', 'Thor']);
+
+    // ... con el operador spread mantengo las categorías anteriores y agrego una nueva
+    const handleAdd = () => {
+        // setCategories([...categories, 'Visión']);
+        // También lo puedo hacer con un callback, devolviendo un nuevo arreglo con todas las categorías
+        setCategories(cats => [...cats, 'Visión']); 
+    };
+
     return (
         <div>
             <h2>GifExpertApp</h2>
             <hr/>
+
+            <button onClick= { handleAdd }>Agregar</button>
+
+            <ol>
+                {
+                    categories.map( category => {
+                        return <li key= { category }> {category} </li>
+                    })
+                }
+            </ol>
         </div>
     )
 }
