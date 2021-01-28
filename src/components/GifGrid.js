9232d3e81d9c03688fbc4905d175d440de3037a8
Adriana Venegas <adrivegas08@gmail.com>
2021-01-28 10:27:54 -0500

    useFetchGifs - obtener imágenes y bandera de carga

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import { useFetchGifs } from '../hooks/useFetchGifs';
-// import { getGifs } from '../helpers/getGifs';
-// import { GifGridItem } from './GifGridItem';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ( { category } ) => {
 
-    // const [images, setImages] = useState([]);
-    const { loading } = useFetchGifs();
-
-    // con [] useEffect solo se va a disparar una vez
-    // solo se ejecuta getGifs() cuando el componente es renderizado por primera vez
-
-    // useEffect(() => {
-    //     getGifs( category )
-    //         .then( setImages );
-    // }, [ category ]); 
-    // [ category ] si la categoría cambia entonces va a volver a ejecutar useEffect y disparar esa petición http
+    const { data:images, loading } = useFetchGifs( category );
 
     return (
         <>
             <h3> { category} </h3> 
-            { loading ? 'Cargando...' : 'Data cargada' }
-            {/* <div className= 'card-grid'>                
+            { loading && <p>Loading</p> }
+            <div className= 'card-grid'>                
                 {
                     images.map( img => (
                         <GifGridItem
@@ -30,7 +19,7 @@ export const GifGrid = ( { category } ) => {
                         />                        
                     ))
                 }
-            </div> */}
+            </div>
         </>        
     )
 }
