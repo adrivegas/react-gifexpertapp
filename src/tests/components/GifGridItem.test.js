bbe01ca9ea96049685f97eb2427d40eeb13f4426
Adriana Venegas <adrivegas08@gmail.com>
2021-02-03 17:21:24 -0500

    Configuración de ambiente de pruebas con enzyme y enzyme-to-json

---
+++ src/tests/components/GifGridItem.test.js
@@ -0,0 +1,11 @@
+import { shallow } from 'enzyme';
+import { GifGridItem } from '../../components/GifGridItem';
+
+describe('Pruebas en <GifGridItem />', () => {
+    
+    test('Debe de mostrar el componente correctamente', () => {
+        const wrapper = shallow( <GifGridItem /> )
+        expect( wrapper ).toMatchSnapshot();
+    })
+    
+})
