f42be7c8551c74655cb4825e46ca9ffdeda4bf9d
Adriana Venegas <adrivegas08@gmail.com>
2021-02-03 17:40:47 -0500

    Se implementa PropTypes y se actualiza snapshot en GifGridItem component

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -3,8 +3,11 @@ import { GifGridItem } from '../../components/GifGridItem';
 
 describe('Pruebas en <GifGridItem />', () => {
     
+    const title = 'Probando GifGridItem';
+    const url = 'https://localhost/gif';
+
     test('Debe de mostrar el componente correctamente', () => {
-        const wrapper = shallow( <GifGridItem /> )
+        const wrapper = shallow( <GifGridItem title={ title } url={ url }/> )
         expect( wrapper ).toMatchSnapshot();
     })
     
