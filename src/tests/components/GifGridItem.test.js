a28b01f5077cc13fbb9b06d2acf5e922d8985f2d
Adriana Venegas <adrivegas08@gmail.com>
2021-02-03 18:09:17 -0500

    Se implementan nuevas pruebas en GifGridItem component

--- src/tests/components/GifGridItem.test.js
+++ src/tests/components/GifGridItem.test.js
@@ -5,10 +5,37 @@ describe('Pruebas en <GifGridItem />', () => {
     
     const title = 'Probando GifGridItem';
     const url = 'https://localhost/gif';
+    const wrapper = shallow( <GifGridItem title={ title } url={ url }/> );
 
     test('Debe de mostrar el componente correctamente', () => {
-        const wrapper = shallow( <GifGridItem title={ title } url={ url }/> )
+        
         expect( wrapper ).toMatchSnapshot();
+
     })
+
+    test('Debe tener el title dentro de un párrafo ', () => {
+        
+        const p = wrapper.find('p');
+        expect( p.text().trim() ).toBe( title );
+
+    })
+    
+    test('Debe tener una imágen con el title y la url de los props', () => {
+        
+        const img = wrapper.find('img');
+        expect( img.prop('src') ).toBe( url );
+        expect( img.prop('alt') ).toBe( title )
+
+    })
+    
+    test('Debe tener la clase animate__fadeIn dentro del div', () => {
+        
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+
+        expect( className.includes('animate__fadeIn') ).toBe( true );
+
+    })
+    
     
 })
