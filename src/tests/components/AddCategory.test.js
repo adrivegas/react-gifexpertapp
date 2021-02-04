16f4ff49845cd374f45f7ae34305f1270aa43057
Adriana Venegas <adrivegas08@gmail.com>
2021-02-04 14:24:50 -0500

    Simular cambios en input de AddCategory component

--- src/tests/components/AddCategory.test.js
+++ src/tests/components/AddCategory.test.js
@@ -4,12 +4,22 @@ import { AddCategory } from '../../components/AddCategory';
 describe('Pruebas en <AddCategory />', () => {
     
     const setCategories = () => {};
+    const wrapper = shallow( <AddCategory setCategories={setCategories} /> );
 
-    test('debe mostrarse correctamente', () => {
+    test('debe mostrarse correctamente', () => {        
+        expect( wrapper ).toMatchSnapshot();        
+    })
 
-        const wrapper = shallow( <AddCategory setCategories={setCategories}/> );
-        expect( wrapper ).toMatchSnapshot();
+    test('debe cambiar la caja de texto', () => {
+
+        const input = wrapper.find('input');
+        const value = 'Hola Mundo';
+
+        input.simulate('change', { target: { value }});
+
+        expect( wrapper.find('p').text().trim() ).toBe( value );
         
     })
+    
     
 })
